bdcca476f80d01c6c712c7fa787fb4da6d8d612d
Birlak <hugues.wieczorek@gmail.com>
2020-12-07 14:29:52 +0100

    add tabs to switch between translator and block mode (block cannot be interverted yet)

--- app/src/main/java/fr/enssat/babelblock/sueur_wieczorek/MainActivity.kt
+++ app/src/main/java/fr/enssat/babelblock/sueur_wieczorek/MainActivity.kt
@@ -1,12 +1,66 @@
 package fr.enssat.babelblock.sueur_wieczorek
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.BlendModeColorFilterCompat
+import androidx.core.graphics.BlendModeCompat
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+import kotlinx.android.synthetic.main.main_activity.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+
+
+        // Tabs Customization
+        tab_layout.setSelectedTabIndicatorColor(Color.BLACK)
+        tab_layout.setBackgroundColor(
+            ContextCompat.getColor(
+                this,
+                R.color.design_default_color_primary
+            )
+        )
+        tab_layout.tabTextColors = ContextCompat.getColorStateList(this, android.R.color.white)
+
+        // Number Of Tabs
+        val numberOfTabs = 2
+
+        // Set Tabs in the center
+//        tab_layout.tabGravity = TabLayout.GRAVITY_CENTER
+
+        // Show all Tabs in screen
+        tab_layout.tabMode = TabLayout.MODE_FIXED
+
+
+        // Set the ViewPager Adapter
+        val adapter = TabsPagerAdapter(supportFragmentManager, lifecycle, numberOfTabs)
+        tabs_viewpager.adapter = adapter
+
+        // Enable Swipe
+        tabs_viewpager.isUserInputEnabled = false
+        // Link the TabLayout and the ViewPager2 together and Set Text & Icons
+        TabLayoutMediator(tab_layout, tabs_viewpager) { tab, position ->
+            when (position) {
+                0 -> {
+                    tab.text = "Translate"
+
+                }
+                1 -> {
+                    tab.text = "Blocks"
+                }
+
+            }
+            // Change color of the icons
+            tab.icon?.colorFilter =
+                BlendModeColorFilterCompat.createBlendModeColorFilterCompat(
+                    Color.WHITE,
+                    BlendModeCompat.SRC_ATOP
+                )
+        }.attach()
     }
 }
